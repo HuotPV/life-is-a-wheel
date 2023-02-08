85e83b47f11158c7cc9a9b379fa4051937624f1f
phuot <pvincent.huot@gmail.com>
2023-02-08 14:26:38 +0100

    Added the main function.

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "World.hpp"
+#include "Cell.hpp"
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    srand(time(0));
+
+    World myWorld;
+    cout << "myWorld successfully initialized ! " << endl;
+
+    myWorld.createNeighbourhood();
+    cout << "Neighbourhood created !" << endl;
+    cout << "End of initialization." << endl;
+    cout << endl;
+
+    cout << "Initial state: " << endl;
+    myWorld.printWorld();
+    cout << endl;
+
+    cout << "Run !" << endl;
+
+    for (int tStep=0; tStep<4; tStep++)
+    {
+        cout << "Time step: " << tStep << endl;
+        myWorld.advance();
+        myWorld.printWorld();
+        cout << "----" << endl;
+    }
+
+    cout << "End of run !" << endl;
+}
