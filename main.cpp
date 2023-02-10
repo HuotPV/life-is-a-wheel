6258ea009ffff8dc3531e310087b2f788bd8d662
phuot <pvincent.huot@gmail.com>
2023-02-10 11:51:40 +0100

    Cleared the main from old code.

--- main.cpp
+++ main.cpp
@@ -16,28 +16,4 @@ int main()
 
     myBatchOfWorlds.SimulateAll(myBatchOfWorlds);
 
-
-    /*
-    myWorld.createNeighbourhood();
-    cout << "Neighbourhood created !" << endl;
-    cout << "End of initialization." << endl;
-    cout << endl;
-
-    cout << "Initial state: " << endl;
-    myWorld.printWorld();
-    cout << endl;
-
-    cout << "Run !" << endl;
-
-    for (int tStep=0; tStep<4; tStep++)
-    {
-        cout << "Time step: " << tStep << endl;
-        myWorld.nextStep();
-        myWorld.printWorld();
-        cout << "----" << endl;
-        myWorld.printWorldFile();
-    }
-
-    cout << "End of run !" << endl;
-    */
 }
